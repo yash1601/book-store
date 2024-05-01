17535915195c5570686681f0cb7684b6ea8c2278
Yash Goplani <yash.goplani@mastercard.com>
2024-05-01 17:58:08 +0530

    changes for websocket

---
+++ src/main/java/flip/Dto/ChatMessage.java
@@ -0,0 +1,15 @@
+package flip.Dto;
+import java.util.Date;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class ChatMessage {
+    private String nickname;
+    private String content;
+    private Date timestamp;
+
+    // Constructors, getters, and setters
+}
